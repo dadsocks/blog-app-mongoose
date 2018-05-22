599fd622d63838a2c3f7be618bae5d9171a5389d
Bill Taggart <taggarw@gmail.com>
2018-05-21 19:31:07 -0700

    database update

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.json());
 
-app.get('/blogPosts', (req, res) => {
+app.get('/posts', (req, res) => {
   BlogPost
     .find()
-    .then(blogPosts => {
+    .then(posts => {
       res.status(200).json({
-        blogPosts: blogPosts.map(
-          (blogPost) => blogPost.serialize())
+        posts: posts.map(
+          (post) => post.serialize())
       });
     })
     .catch( err => {
@@ -28,17 +28,18 @@ app.get('/blogPosts', (req, res) => {
     });
 });
 
-app.get('/blogPosts/:id', (req, res) => {
+
+app.get('/posts/:id', (req, res) => {
   BlogPost
     .findById(req.params.id)
-    .then(blogPost => res.json(blogPost.serialize()))
+    .then(post => res.json(post.serialize()))
     .catch( err => {
       console.error(err);
       res.status(500).json({ message: "Internal Service Errror"});
     });
 });
 
-app.post('/blogPosts', (req, res) => {
+app.post('/posts', (req, res) => {
   const requiredFields = ['title','content','author'];
 
   for (let i = 0; i < requiredFields.length; i++) {
@@ -50,21 +51,21 @@ app.post('/blogPosts', (req, res) => {
     }
   }
 
-  blogPost
+  BlogPost
     .create({
       title: req.body.title,
       content: req.body.content,
       author: req.body.author,
       created: req.body.created
     })
-    .then(blogPost => res.status(201).json(blogPost.serialize()))
+    .then(post => res.status(201).json(post.serialize()))
     .catch(err => {
       console.error(err);
       res.status(500).json( { message: "Internal Service Error" });
     });
 });
 
-app.put('/blogPosts/:id', (req,res) => {
+app.put('/posts/:id', (req,res) => {
   if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match`;
     console.error(message);
@@ -82,14 +83,14 @@ app.put('/blogPosts/:id', (req,res) => {
 
   BlogPost
     .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-    .then(blogPost => res.status(204).end())
+    .then(post => res.status(204).end())
     .catch(err => res.status(500).json({ message: "Internal Service Error" }));
 });
 
-app.delete('/blogPosts/:id', (req,res) => {
+app.delete('/posts/:id', (req,res) => {
   BlogPost
     .findByIdAndRemove(req.params.id)
-    .then(blogPost => res.status(204).end())
+    .then(post => res.status(204).end())
     .catch(err => res.status(500).json({ message: "Internal Service Error" }));
 });
 
