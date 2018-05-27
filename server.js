6748fe66b67986bef17cd34d3851f7966012f44b
Bill Taggart <taggarw@gmail.com>
2018-05-27 08:26:37 -0700

    Correct Server Errors and DB connection errors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const { DATABASE_URL, PORT } = require('./config');
+const { DATABASE_URL,PORT } = require('./config');
 const { BlogPost } = require('./models');
 
 const app = express();
@@ -13,93 +13,96 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.json());
 
-app.get('/posts', (req, res) => {
+//Get posts
+app.get('/posts', (req,res) => {
   BlogPost
-    .find()
-    .then(posts => {
-      res.status(200).json({
-        posts: posts.map(
-          (post) => post.serialize())
-      });
-    })
-    .catch( err => {
-      console.error(err);
-      res.status(500).json({ message: "Internal Service Error"});
-    });
+  .find()
+  .then(posts => {
+    res.status(200).json(posts.map(post => post.serialize()));
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json('Something ent wrong , papi');
+  });
 });
 
-
-app.get('/posts/:id', (req, res) => {
+//Get posts by id
+app.get('/posts/:id', (req,res) => {
   BlogPost
-    .findById(req.params.id)
-    .then(post => res.json(post.serialize()))
-    .catch( err => {
-      console.error(err);
-      res.status(500).json({ message: "Internal Service Errror"});
-    });
+  .findByID(req.params.id)
+  .then(post => res.json(post.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json('Something ent wrong , papi');
+  });
 });
 
-app.post('/posts', (req, res) => {
+//Create Request
+app.post('/posts', (req,res) => {
   const requiredFields = ['title','content','author'];
-
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing "${field}" in request body`;
+    if(!(field in req.body)) {
+      const message = `You're missing ${field} in the request, papi.`;
       console.error(message);
-      res.status(500).json( {message: "Internal Service Error"});
+      return res.status(400).send(message);
     }
   }
 
   BlogPost
-    .create({
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author,
-      created: req.body.created
-    })
-    .then(post => res.status(201).json(post.serialize()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json( { message: "Internal Service Error" });
-    });
+  .create({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  })
+  .then(post => res.status(201).json(post.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json('Something ent wrong , papi');
+  });
 });
 
+//Update
 app.put('/posts/:id', (req,res) => {
   if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match`;
-    console.error(message);
-    return res.status(400).json({ message: message });
+    res.status(400).json({error: 'ID was not found, papi.'});
   }
 
-  const toUpdate = {};
-  const updateableFields = ['title','content','author'];
-
-  updateableFields.forEach(field => {
-    if(field in req.body) {
-      toUpdate[field] = req.body[field];
+  const updatedPost = {};
+  const youCanUpdate = ['title','content','author'];
+  youCanUpdate.forEach(field => {
+    if (field in req.body) {
+      updatedPost[field] = req.body[field];
     }
   });
 
   BlogPost
-    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({ message: "Internal Service Error" }));
+  .findByIdAndUpdate(req.params.id, {$set: updatedPost}, {new: true})
+  .then(updatedPost => res.status(204).json({message:'Update successful'}))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json('Something ent wrong , papi');
+  });
 });
 
+//Delete
 app.delete('/posts/:id', (req,res) => {
   BlogPost
-    .findByIdAndRemove(req.params.id)
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({ message: "Internal Service Error" }));
+  .findByIdAndRemove(req.params.id)
+  .then(res.status(204).json({message:'Deletion was a success'}))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json('Something ent wrong , papi');
+  });
 });
 
-app.use('*', (req,res) => {
+app.use('*', function (req, res) {
   res.status(404).json({ message: 'Not Found' });
 });
 
 let server;
 
+// this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, err => {
@@ -118,6 +121,8 @@ function runServer(databaseUrl, port = PORT) {
   });
 }
 
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
@@ -132,6 +137,8 @@ function closeServer() {
   });
 }
 
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
